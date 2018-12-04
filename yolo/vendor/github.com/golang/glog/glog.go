433ff925129fe9917403e3859a1c3c523dd5007a
Joseph Burnett <josephburnett79@gmail.com>
2018-12-04 16:41:14 -0500

    Hack out conflicting flags.

--- yolo/vendor/github.com/golang/glog/glog.go
+++ yolo/vendor/github.com/golang/glog/glog.go
@@ -396,12 +396,12 @@ type flushSyncWriter interface {
 }
 
 func init() {
-	flag.BoolVar(&logging.toStderr, "logtostderr", false, "log to standard error instead of files")
-	flag.BoolVar(&logging.alsoToStderr, "alsologtostderr", false, "log to standard error as well as files")
-	flag.Var(&logging.verbosity, "v", "log level for V logs")
-	flag.Var(&logging.stderrThreshold, "stderrthreshold", "logs at or above this threshold go to stderr")
-	flag.Var(&logging.vmodule, "vmodule", "comma-separated list of pattern=N settings for file-filtered logging")
-	flag.Var(&logging.traceLocation, "log_backtrace_at", "when logging hits line file:N, emit a stack trace")
+	flag.BoolVar(&logging.toStderr, "logtostderr_hack", false, "log to standard error instead of files")
+	flag.BoolVar(&logging.alsoToStderr, "alsologtostderr_hack", false, "log to standard error as well as files")
+	flag.Var(&logging.verbosity, "v_hack", "log level for V logs")
+	flag.Var(&logging.stderrThreshold, "stderrthreshold_hack", "logs at or above this threshold go to stderr")
+	flag.Var(&logging.vmodule, "vmodule_hack", "comma-separated list of pattern=N settings for file-filtered logging")
+	flag.Var(&logging.traceLocation, "log_backtrace_at_hack", "when logging hits line file:N, emit a stack trace")
 
 	// Default stderrThreshold is ERROR.
 	logging.stderrThreshold = errorLog
